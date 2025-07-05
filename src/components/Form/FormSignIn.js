ee919a3cb3e7e9364a0af3590b39face7c32ff6e
coder-sys <unix99493@gmail.com>
2025-07-05 17:08:58 -0500

    f this

--- src/components/Form/FormSignIn.js
+++ src/components/Form/FormSignIn.js
@@ -41,7 +41,8 @@ const FormSignIn = () => {
 
 		if (white_list.includes(disected_address)) {
 			try {
-				let api = await fetch(`https://omniboard-apis.afd.enterprises/sign_in/${firstname}/${lastname}/${__password__}/${__email__}`);
+				let api = await fetch(`${DOMAIN}/sign_in/${firstname}/${lastname}/${__password__}/${__email__}`)
+				let api_json = await api.json();
 				window.location.replace(`${SD1}/login`);
 				return api_json;
 			} catch (error) {
